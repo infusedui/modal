69311e344a03e31d12dc08abc1ef187b9c3e3435
Jérémie Meunier <jeremie.meunier74@gmail.com>
2024-06-04 18:52:55 +0200

    add modal component

---
+++ src/components/modal/Modal.types.ts
@@ -0,0 +1,30 @@
+export interface ModalProps {
+  children: React.ReactNode;
+}
+
+export interface CloseProps {
+  setModalVisibility: React.Dispatch<React.SetStateAction<boolean>>;
+  refreshing?: boolean;
+  refreshHandler?: () => void;
+}
+
+export interface TitleProps {
+  children: React.ReactNode;
+}
+
+export interface ModalContainerProps {
+  children: React.ReactNode;
+  size?: "small" | "medium" | "large" | "fullscreen";
+  template?: "menu" | undefined;
+}
+
+export interface BodyProps {
+  children: React.ReactNode;
+}
+
+export interface NavigationItemProps {
+  label: string;
+  setModalPage: React.Dispatch<React.SetStateAction<string | number>>;
+  isActive: boolean;
+  id: string | number;
+}
